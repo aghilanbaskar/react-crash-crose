adf6388490242ae819ee75bca2e64d9a123218c4
Aghilan Baskar <42873145+aghilanbaskar@users.noreply.github.com>
2024-06-25 21:56:57 +0530

    chore: initialized

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+
+interface CardProps {
+    children: ReactNode,
+    bg?: string
+}
+
+const Card = ({
+    children,
+    bg = "bg-gray-100"
+}: CardProps) => {
+  return (
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>
+        {children}
+    </div>
+  )
+}
+
+export default Card
